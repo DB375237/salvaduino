2d1116dcb66918b68e7c121fa6ebe1024a54f208
dario <dario.bellisario@gmail.com>
2018-12-20 00:05:33 +0100

    .

---
+++ libraries/SolenoidLock_1.0/SolenoidLock.cpp
@@ -0,0 +1,22 @@
+/*
+  Dario B. mat. 375237
+  SolenoidLock.cpp - Library for abstracting a solenoid lock
+  (positive logic - high logic level activates solenoid).
+  Released into the public domain (CC0 1.0 Universal).
+*/
+
+#include "Arduino.h"
+#include "SolenoidLock.h"
+
+SolenoidLock::SolenoidLock(int pin)
+{
+	pinMode(pin, OUTPUT);
+  _pin = pin;
+}
+
+void SolenoidLock::release()
+{
+  digitalWrite(_pin, HIGH);
+  delay(RELEASE_DURATION);
+  digitalWrite(_pin, LOW);
+}
---
+++ libraries/SolenoidLock_1.0/SolenoidLock.h
@@ -0,0 +1,22 @@
+/*
+  Dario B. mat. 375237
+  SolenoidLock.h - Library for abstracting a solenoid lock
+  (positive logic - high logic level activates solenoid).
+  Released into the public domain (CC0 1.0 Universal).
+*/
+#ifndef SolenoidLock_h
+#define SolenoidLock_h
+
+#include "Arduino.h"
+#define RELEASE_DURATION 500
+
+class SolenoidLock
+{
+  public:
+    SolenoidLock(int pin);
+    void release();
+  private:
+    int _pin;
+};
+
+#endif
